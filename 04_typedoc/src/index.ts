0364287396ad8c33c8fed6765cd9c2daccf50a8c
Jinga <info.alpain@gmail.com>
2023-03-27 18:19:49 +0900

    <add> initial commit

---
+++ 04_typedoc/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 2つの数値の和を返す関数
+ *
+ * @param {number} a - 数値1
+ * @param {number} b - 数値2
+ * @returns {number} 和
+ */
+function add(a: number, b: number) {
+  return a + b;
+}
+const n = add(1, 2);
+console.log(n);
