83b78dba9d0b0d5567d4b7cef6ef6d6c87431350
Jinga <info.alpain@gmail.com>
2023-03-30 14:36:34 +0900

    <add> initial commit

--- 06_read_geojson_stream/src/index.ts
+++ 06_read_geojson_stream/src/index.ts
@@ -1,32 +1,24 @@
-import StreamChain from "stream-chain";
 import StreamJSON from "stream-json";
 import Pick from "stream-json/filters/Pick.js";
 import StreamArray from "stream-json/streamers/StreamArray.js";
 import * as fs from "fs";
-
-// GeoJSON FeaturesCollectionのStream読み込み
-function readGeoJSONStream() {
-  const geojsonFilename = process.argv[2];
-  const pipeline = StreamChain.chain([
-    fs.createReadStream(geojsonFilename),
-    StreamJSON.parser(),
-    Pick.pick({ filter: "features" }),
-    StreamArray.streamArray(),
-    (data) => {
-      const value = data.value;
-      console.log(JSON.stringify(value.geometry.coordinates));
-      return value;
-    },
-  ]);
-
-  let counter = 0;
-  pipeline.on("data", () => ++counter);
-  pipeline.on("end", () => console.log(counter));
-}
 
 // エントリポイント
 if (process.argv.length != 3) {
   console.log("Usage: Set 1 argument [GeoJSON File]");
   process.exit();
 }
-readGeoJSONStream();
+
+// GeoJSON FeaturesCollectionのStream読み込み
+const streamReader = async (streamArray: StreamArray) => {
+  for await (const sa of streamArray) {
+    console.log(sa);
+  }
+};
+const streamArray = fs
+  .createReadStream(process.argv[2])
+  .pipe(StreamJSON.parser())
+  .pipe(Pick.pick({ filter: "features" }))
+  .pipe(StreamArray.streamArray());
+await streamReader(streamArray);
+console.log("OK");
